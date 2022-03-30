79f3d903c4839d621d1b46c351b499c61b9909a2
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2022-03-30 18:14:46 +0100

    first commit

---
+++ hello.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+	if name == "" {
+		return englishHelloPrefix + "World"
+	} else {
+		return englishHelloPrefix + name
+	}
+}
+
+
+func main() {
+	fmt.Println(Hello("world"))
+}
