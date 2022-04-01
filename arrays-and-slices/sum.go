ae8f7213190832816dcfd315d7ffe0708a9572d9
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2022-04-01 17:50:41 +0100

    arrays and slices

---
+++ arrays-and-slices/sum.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Sum(numbers [5]int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func main () {
+	array := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(Sum(array))
+}
